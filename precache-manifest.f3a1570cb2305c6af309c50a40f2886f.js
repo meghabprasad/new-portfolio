db79b3f513e8f9bcdab59ff5b9d53c4d21f8861a
Megha Prasad Bindiganavale <meghab.prasad@gmail.com>
2019-08-06 11:30:45 -0700

    Updates

---
+++ precache-manifest.f3a1570cb2305c6af309c50a40f2886f.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e918f521483accae7331414d519a302c",
+    "url": "/new-portfolio/index.html"
+  },
+  {
+    "revision": "c57657b18e766c6c7589",
+    "url": "/new-portfolio/static/css/main.a69f1e2a.chunk.css"
+  },
+  {
+    "revision": "4c3a8de9a1508a9f3ce4",
+    "url": "/new-portfolio/static/js/2.ec34d09b.chunk.js"
+  },
+  {
+    "revision": "c57657b18e766c6c7589",
+    "url": "/new-portfolio/static/js/main.0bb76245.chunk.js"
+  },
+  {
+    "revision": "de2e177033eebf0f5ecf",
+    "url": "/new-portfolio/static/js/runtime~main.69dd43ee.js"
+  },
+  {
+    "revision": "2884dff613015f476ab561b12e895320",
+    "url": "/new-portfolio/static/media/ARVO-img.2884dff6.jpg"
+  },
+  {
+    "revision": "5149487cedb5e506676c2107d3e85540",
+    "url": "/new-portfolio/static/media/ARVO_Posterfinal.5149487c.pdf"
+  },
+  {
+    "revision": "8a2ee7d4dfe794227088875ec3b6388f",
+    "url": "/new-portfolio/static/media/ajax-logo.png.8a2ee7d4.png"
+  },
+  {
+    "revision": "ce245bbf29628572811a71ae3e0e882d",
+    "url": "/new-portfolio/static/media/books.ce245bbf.gif"
+  },
+  {
+    "revision": "cdf4b4599d51803a0f0f7d928bf7e66f",
+    "url": "/new-portfolio/static/media/bootstrap-logo.cdf4b459.png"
+  },
+  {
+    "revision": "19e7a22886f62cadadce5e6e2c44fbf4",
+    "url": "/new-portfolio/static/media/emogifs.19e7a228.gif"
+  },
+  {
+    "revision": "72d832f72a0aacbebb66dfc4124c0597",
+    "url": "/new-portfolio/static/media/finfo.72d832f7.gif"
+  },
+  {
+    "revision": "c24b6b9c0fcd84c7b258879880472660",
+    "url": "/new-portfolio/static/media/firebase-logo.c24b6b9c.png"
+  },
+  {
+    "revision": "975a3422c5ae0957cb71f944dc0b7e6c",
+    "url": "/new-portfolio/static/media/github-sign.975a3422.png"
+  },
+  {
+    "revision": "305dbeaafe27cb4594ea80ce9b66ce92",
+    "url": "/new-portfolio/static/media/gymate.305dbeaa.gif"
+  },
+  {
+    "revision": "a99f946218608bff3ff68a4d8913993d",
+    "url": "/new-portfolio/static/media/happy-face.a99f9462.jpg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/new-portfolio/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "cb2f65a2181811ba82d3db0ed0880d1c",
+    "url": "/new-portfolio/static/media/material-ui-logo.cb2f65a2.svg"
+  },
+  {
+    "revision": "0df3ec686a428a23814a918318942d92",
+    "url": "/new-portfolio/static/media/materialize-logo.0df3ec68.png"
+  },
+  {
+    "revision": "db2aca772e517d9666e2ccd7d4cf1e3f",
+    "url": "/new-portfolio/static/media/megha-resume.db2aca77.pdf"
+  },
+  {
+    "revision": "3d40302b0864db8e2210e295152d8543",
+    "url": "/new-portfolio/static/media/mongo-logo.3d40302b.png"
+  },
+  {
+    "revision": "08ebf3d0bdc551aa5861bb101c767dd2",
+    "url": "/new-portfolio/static/media/node-logo.08ebf3d0.png"
+  },
+  {
+    "revision": "7c0eef6d557704b15e0d8c4bc876e644",
+    "url": "/new-portfolio/static/media/peak.7c0eef6d.gif"
+  },
+  {
+    "revision": "692adb35708d9279ca7eb56c71bfc102",
+    "url": "/new-portfolio/static/media/react-logo.692adb35.png"
+  },
+  {
+    "revision": "01bd24c27903d876d1fc07aa0182efdd",
+    "url": "/new-portfolio/static/media/scrape.01bd24c2.gif"
+  },
+  {
+    "revision": "02a7a2312e1e1552594b797ca2b96a6a",
+    "url": "/new-portfolio/static/media/stanford-logo.02a7a231.png"
+  }
+]);
